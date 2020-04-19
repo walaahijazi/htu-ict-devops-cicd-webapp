86fcf7aff39cf1c924c7938a292862c78ea37bcc
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-19 18:02:07 +0300

    Update setup.sh
    pip

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 !#/bin/bash
 sudo wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz
 
+python3 -m pip install --user virtualenv
+
+
 sudo apt install gunicorn3
 
 pip install -r requirements.txt
