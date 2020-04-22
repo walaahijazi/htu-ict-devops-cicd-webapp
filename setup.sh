fcf53cb517eb8a43d3bc3465ed1ba41729915aee
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-22 09:51:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 !#/bin/bash
 sudo apt-get install python3-venv
 sudo apt-get install pthon3-pip
-python3 -m venv test_inv
-source test_inv/bin/activate
-pip3 install -r requirements.txt
-pip3 install gunicorn
+python3 -m venv test_inv && source test_inv/bin/activate && pip3 install -r requirements.txt && pip3 install gunicorn
+
+
+
 gunicorn --bind=0.0.0.0:5000 --workers=4 hello:app
