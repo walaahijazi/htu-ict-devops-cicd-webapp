8da7a90cc89e76eee35e10df7f9ee3d3d6e3efc4
walaahijazi <62707740+walaahijazi@users.noreply.github.com>
2020-04-22 09:02:06 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,16 @@ sudo wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz
 
 sudo apt install gunicorn3
 
-python3 -m venv
-source . /bin/activate
+pip install gunicorn flask
+
+python3 -m pip install --user virtualenv
+
+alias activate=". ../.env/bin/activate"
+
+
 
 pip install -r requirements.txt
 
-gunicorn --bind=0.0.0.0:5000 --workers=4 
 
-
+gunicorn --bind=0.0.0.0:5000 --workers=4 hello:app
 
